af159377da875ba1663c476bc938955896ef08bb
Giuseppe Mazzapica <giuseppe.mazzapica@gmail.com>
2014-08-28 13:01:14 +0200

    Tests for magic methods

--- tests/tests/FaberTest.php
+++ tests/tests/FaberTest.php
@@ -2,9 +2,33 @@
 
 class FaberTest extends TestCase {
 
-    function testToString() {
+    function testMagicToString() {
         $faber = $this->getFaber( 'foo' );
         assertEquals( "{$faber}", "GM\Faber foo" );
+    }
+
+    function testMagicCall() {
+        $faber = $this->getFaber( 'foo' );
+        $faber['foo'] = 'bar';
+        assertEquals( $faber->getFoo(), "bar" );
+    }
+
+    function testMagicCallError() {
+        $faber = $this->getFaber( 'foo' );
+        $faber['foo'] = 'bar';
+        assertInstanceOf( 'WP_Error', $faber->getBar() );
+    }
+
+    function testMacicSet() {
+        $faber = $this->getFaber( 'foo' );
+        $faber->foo = 'bar';
+        assertEquals( $faber['foo'], 'bar' );
+    }
+
+    function testMacicGet() {
+        $faber = $this->getFaber( 'foo' );
+        $faber['foo'] = 'bar';
+        assertEquals( $faber->foo, 'bar' );
     }
 
 }
