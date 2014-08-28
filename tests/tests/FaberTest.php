4e622eccd30fca4f99ef6507d9d297138892a5ef
Giuseppe Mazzapica <giuseppe.mazzapica@gmail.com>
2014-08-28 13:41:05 +0200

    More tests

--- tests/tests/FaberTest.php
+++ tests/tests/FaberTest.php
@@ -31,4 +31,68 @@ class FaberTest extends TestCase {
         assertEquals( $faber->foo, 'bar' );
     }
 
+    function testSetId() {
+        $faber = $this->getFaber( 'foo' );
+        $setted = $faber->setId( 'bar' );
+        assertEquals( 'foo', $faber->getId() );
+        assertTrue( $setted === $faber );
+    }
+
+    function testLoad() {
+        $faber = $this->getFaber( 'foo' );
+        $closure = function() {
+            return new \WP_Error;
+        };
+        $data = [
+            'foo'   => 'bar',
+            'bar'   => 'baz',
+            'error' => $closure
+        ];
+        $load = $faber->load( $data );
+        assertEquals( $faber['foo'], 'bar' );
+        assertEquals( $faber['bar'], 'baz' );
+        assertInstanceOf( 'WP_Error', $faber['error'] );
+        assertTrue( $load === $faber );
+    }
+
+    function testLoadFile() {
+        $faber = $this->getFaber( 'foo' );
+        $load = $faber->loadFile( FABERPATH . '/tests/helpers/array_example.php' );
+        assertEquals( $faber['foo'], 'bar' );
+        assertEquals( $faber['bar'], 'baz' );
+        assertInstanceOf( 'WP_Error', $faber['error'] );
+        assertTrue( $load === $faber );
+    }
+
+    function testLoadFileError() {
+        $faber = $this->getFaber( 'foo' );
+        $load = $faber->loadFile( FABERPATH . '/tests/helpers/i-do-not-exists.php' );
+        assertInstanceOf( 'WP_Error', $load );
+    }
+
+    function testAdd() {
+        $faber = $this->getFaber( 'foo' );
+        $closure = function() {
+            return new \WP_Error;
+        };
+        $faber->add( 'foo', 'bar' );
+        $faber->add( 'bar', 'baz' );
+        $faber->add( 'error', $closure );
+        $add = $faber->add( 'bar', 'I never exists' );
+        assertEquals( $faber['foo'], 'bar' );
+        assertEquals( $faber['bar'], 'baz' );
+        assertInstanceOf( 'WP_Error', $faber['error'] );
+        assertTrue( $add === $faber );
+    }
+
+    function testProtect() {
+        $faber = $this->getFaber( 'foo' );
+        $closure = function() {
+            return new \WP_Error;
+        };
+        $protect = $faber->protect( 'my_closure', $closure );
+        assertEquals( $closure, $faber['my_closure'] );
+        assertTrue( $protect === $faber );
+    }
+
 }
