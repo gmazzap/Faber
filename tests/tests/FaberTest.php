b97b6678750c3d15bb9842a87eac332dff883d0e
Giuseppe Mazzapica <giuseppe.mazzapica@gmail.com>
2014-08-28 14:54:34 +0200

    Tests for get()

--- tests/tests/FaberTest.php
+++ tests/tests/FaberTest.php
@@ -95,4 +95,66 @@ class FaberTest extends TestCase {
         assertTrue( $protect === $faber );
     }
 
+    function testGetWhenProp() {
+        $faber = $this->getFaber( 'foo' );
+        $faber['foo'] = 'bar';
+        assertEquals( $faber->get( 'foo' ), 'bar' );
+    }
+
+    function testGetWhenFactory() {
+        $faber = $this->getFaber( 'foo' );
+        $faber['stub2'] = function( $c, $args = [ ] ) {
+            $stub = new \FaberTestStub;
+            $stub->foo = $c['foo'];
+            $stub->bar = $c['bar'];
+            $stub->args = $args;
+            $stub->sub_stub = $c['stub'];
+            return $stub;
+        };
+        $faber['stub'] = function( $c, $args = [ ] ) {
+            $stub = new \FaberTestStub;
+            $stub->foo = $c['foo'];
+            $stub->bar = $c['bar'];
+            $stub->args = $args;
+            return $stub;
+        };
+        $faber['foo'] = 'bar';
+        $faber['bar'] = 'baz';
+        $stub = $faber->get( 'stub2', [ 'id' => 'stub2' ] );
+        $stub_clone = $faber->get( 'stub2', [ 'id' => 'stub2' ] );
+        $stub_alt = $faber->get( 'stub2', [ 'id' => 'stub_alt' ] );
+        $stub_alt_clone = $faber->get( 'stub2', [ 'id' => 'stub_alt' ] );
+        assertInstanceOf( 'FaberTestStub', $stub );
+        assertInstanceOf( 'FaberTestStub', $stub_clone );
+        assertInstanceOf( 'FaberTestStub', $stub_alt );
+        assertInstanceOf( 'FaberTestStub', $stub->sub_stub );
+        assertTrue( $stub->sub_stub === $faber->get( 'stub' ) );
+        assertTrue( $stub->sub_stub !== $stub );
+        assertEquals( 'stub2', $stub->args['id'] );
+        assertEquals( 'stub_alt', $stub_alt->args['id'] );
+        assertTrue( $stub_clone === $stub );
+        assertTrue( $stub_alt !== $stub );
+        assertTrue( $stub_alt_clone === $stub_alt );
+    }
+
+    function testGetWhenFactoryAndAssure() {
+        $faber = $this->getFaber( 'foo' );
+        $faber['stub'] = function() {
+            return new \FaberTestStub;
+        };
+        $a = $faber->get( 'stub', [ ], 'FaberTestStub' );
+        $b = $faber->get( 'stub', [ ], 'GM\Faber' );
+        assertInstanceOf( 'FaberTestStub', $a );
+        assertInstanceOf( 'WP_Error', $b );
+    }
+
+    function testGetWrongFactory() {
+        $faber = $this->getFaber( 'foo' );
+        $faber['stub'] = function() {
+            return new \FaberTestStub;
+        };
+        $a = $faber->get( 'foo' );
+        assertInstanceOf( 'WP_Error', $a );
+    }
+
 }
