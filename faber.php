ed4e6a710898da4012cfbcb49909ea549fc05f86
Giuseppe Mazzapica <giuseppe.mazzapica@gmail.com>
2014-08-28 11:32:25 +0200

    ok, bootstrapfile make no sense

--- faber.php
+++
@@ -1,7 +0,0 @@
-<?php
-if ( ! class_exists( 'GM\Faber' ) && file_exists( dirname( __FILE__ ) . '/vendor/autoload.php' ) ) {
-    require_once dirname( __FILE__ ) . '/vendor/autoload.php';
-}
-if ( ! class_exists( 'Faber' ) ) {
-    class_alias( 'GM\Faber', 'Faber' );
-}
