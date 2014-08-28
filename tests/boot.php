377e8d9c51cceeb931a496929cfd8c6054348eca
Giuseppe Mazzapica <giuseppe.mazzapica@gmail.com>
2014-08-28 12:54:43 +0200

    Load tests helpers in boot.php, not via composer

--- tests/boot.php
+++ tests/boot.php
@@ -1,6 +1,8 @@
 <?php
 if ( ! defined( 'FABERPATH' ) ) define( 'FABERPATH', dirname( dirname( __FILE__ ) ) );
 
+require_once FABERPATH . '/tests/helpers.php';
+
 require_once FABERPATH . '/vendor/autoload.php';
 
 require_once FABERPATH . '/vendor/phpunit/phpunit/PHPUnit/Framework/Assert/Functions.php';
