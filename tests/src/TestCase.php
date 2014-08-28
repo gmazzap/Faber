31218c0d7695f98a8bc126ac6fb9c78db6f41d82
Giuseppe Mazzapica <giuseppe.mazzapica@gmail.com>
2014-08-28 12:55:32 +0200

    Added HooksMock support in base TestCase

--- tests/src/TestCase.php
+++ tests/src/TestCase.php
@@ -1,6 +1,13 @@
 <?php namespace GM\Faber\Tests;
 
+use Brain\HooksMock\HooksMock as HM;
+
 class TestCase extends \PHPUnit_Framework_TestCase {
+
+    function tearDown() {
+        parent::tearDown();
+        HM::tearDown();
+    }
 
     protected function getFaber( $id, $things = [ ] ) {
         return new \GM\Faber( $things, $id );
