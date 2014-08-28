b7803d48012145acee1fd760a1f29407f5311cfe
Giuseppe Mazzapica <giuseppe.mazzapica@gmail.com>
2014-08-28 12:55:02 +0200

    Add some helpers

--- tests/helpers.php
+++ tests/helpers.php
@@ -1,1 +1,32 @@
 <?php
+if ( ! class_exists( 'WP_Error' ) ) {
+
+    class WP_Error {
+
+        private $errors = [ ];
+
+        function add( $code = '', $message = '', $arguments = '' ) {
+            if ( ! isset( $this->errors[$code] ) ) {
+                $this->errors[$code] = [ ];
+            }
+            $this->errors[$code][] = [ 'code' => $code, 'message' => $message, 'data' => $arguments ];
+        }
+
+    }
+}
+
+if ( ! function_exists( 'apply_filters' ) ) {
+
+    function apply_filters( $filter, $data ) {
+        return $data;
+    }
+
+}
+
+if ( ! function_exists( 'do_action' ) ) {
+
+    function do_action( $action, $data = [ ] ) {
+        return NULL;
+    }
+
+}
