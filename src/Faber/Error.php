c665010d7e3d5526b5d06e2a2500bfaae6b628b1
Giuseppe Mazzapica <giuseppe.mazzapica@gmail.com>
2014-08-30 00:01:27 +0200

    Error implements ArrayAccess

--- src/Faber/Error.php
+++ src/Faber/Error.php
@@ -1,6 +1,6 @@
 <?php namespace GM\Faber;
 
-class Error extends \WP_Error {
+class Error extends \WP_Error implements \ArrayAccess {
 
     function __call( $name, $arguments ) {
         $code = "faber-call-on-error-{$name}";
@@ -11,6 +11,32 @@ class Error extends \WP_Error {
 
     function __toString() {
         return 'ERROR: ' . $this->get_error_message();
+    }
+
+    public function offsetExists( $offset ) {
+        $code = "faber-call-on-error-{$offset}";
+        $message = "Tried to check for {$offset} property on an error object";
+        $this->add( $code, $message );
+        return FALSE;
+    }
+
+    public function offsetGet( $offset ) {
+        $code = "faber-call-on-error-{$offset}";
+        $message = "Tried to get {$offset} from an error object";
+        $this->add( $code, $message );
+        return $this;
+    }
+
+    public function offsetSet( $offset, $value ) {
+        $code = "faber-call-on-error-{$offset}";
+        $message = "Tried to set {$offset} on an error object";
+        $this->add( $code, $message, $value );
+    }
+
+    public function offsetUnset( $offset ) {
+        $code = "faber-call-on-error-{$offset}";
+        $message = "Tried to unset {$offset} from an error object";
+        $this->add( $code, $message );
     }
 
 }
